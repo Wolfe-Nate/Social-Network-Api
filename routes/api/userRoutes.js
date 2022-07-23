60c537a72412a725b0f19aaa5bf5badeeffbfa7b
Nate Wolfe <wolfe.nate26@gmail.com>
2022-07-23 16:42:34 -0600

    routes added in

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addFriends,
+  removeFriends,
+  editUser,
+} = require("../../controllers/userController");
+
+//localhost/api/users
+router.route("/").get(getAllUsers).post(createUser);
+
+router.route("/:userId").get(getSingleUser).delete(deleteUser).put(editUser);
+
+router
+  .route("/:userId/friends/:friendId")
+  .delete(removeFriends)
+  .post(addFriends);
+
+module.exports = router;
